2e3ae4b1ef057dcc2f07fb4d62d0861d5d84807c
shaharar <shaharar@post.bgu.ac.il>
2018-06-12 17:53:46 +0300

    model

--- src/Model/IModel.java
+++ src/Model/IModel.java
@@ -1,4 +1,11 @@
 package Model;
 
+import javafx.scene.input.KeyCode;
+
 public interface IModel {
+    void generateMaze (int rows, int columns);
+    int[][] getMaze ();
+    int getCharacterPositionRow();
+    int getCharacterPositionColumn();
+    void moveCharacter (KeyCode movement);
 }
--- src/Model/MyModel.java
+++ src/Model/MyModel.java
@@ -1,7 +1,39 @@
 package Model;
 
+import Server.Server;
+import javafx.scene.input.KeyCode;
+
 import java.util.Observable;
 
 public class MyModel  extends Observable implements IModel {
 
+    Server server;
+
+    public MyModel() {
+    }
+
+    @Override
+    public void generateMaze(int rows, int columns) {
+
+    }
+
+    @Override
+    public int[][] getMaze() {
+        return new int[0][];
+    }
+
+    @Override
+    public int getCharacterPositionRow() {
+        return 0;
+    }
+
+    @Override
+    public int getCharacterPositionColumn() {
+        return 0;
+    }
+
+    @Override
+    public void moveCharacter(KeyCode movement) {
+
+    }
 }
