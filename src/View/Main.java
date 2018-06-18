1d8e3117bfef8c57f35f5a8b54924600d8f5eb7d
J-morag <38430800+J-morag@users.noreply.github.com>
2018-06-18 08:30:40 +0300

    resized instructions. removed main screen min size

--- src/View/Main.java
+++ src/View/Main.java
@@ -33,8 +33,8 @@ public class Main extends Application {
         Parent root = fxmlLoader.load(getClass().getResource("MyView.fxml").openStream());
         Scene scene = new Scene(root, 800, 650);
         primaryStage.setScene(scene);
-        primaryStage.setMinHeight(475);
-        primaryStage.setMinWidth(550);
+//        primaryStage.setMinHeight(475);
+//        primaryStage.setMinWidth(550);
 
         MyViewController view = fxmlLoader.getController();
         viewC = view;
